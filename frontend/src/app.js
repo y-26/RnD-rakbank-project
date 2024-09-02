1f8a1ae621c7b954b93d59e00f826ed44aefd96f
Raj Sah <93470062+y-26@users.noreply.github.com>
2024-09-02 09:05:38 +0000

    Update app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-function App() {
+function Ap() {
   const [prices, setPrices] = useState([]);
   const [itemName, setItemName] = useState("");
   const [price, setPrice] = useState("");
